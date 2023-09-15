03e1f0b38b4958f91a4dde1b229130ba45b7e9e2
pirosiki197 <lingmukangtai825@gmail.com>
2023-09-15 11:06:36 +0900

    define repository interface

---
+++ pkg/handler/repository.go
@@ -0,0 +1,15 @@
+package handler
+
+import (
+	"context"
+
+	"github.com/pirosiki197/emoine/pkg/model"
+)
+
+type Repository interface {
+	CreateEvent(ctx context.Context, event *model.Event) error
+	GetEvents(ctx context.Context) ([]model.Event, error)
+
+	SendComment(ctx context.Context, comment *model.Comment) error
+	GetComments(ctx context.Context, eventID string) ([]model.Comment, error)
+}
