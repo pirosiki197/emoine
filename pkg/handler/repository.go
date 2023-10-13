21419bc98573b7105d63ce3471765ffa0e8577e4
pirosiki197 <lingmukangtai825@gmail.com>
2023-10-14 02:24:08 +0900

    Use domain model

--- pkg/handler/repository.go
+++ pkg/handler/repository.go
@@ -3,14 +3,14 @@ package handler
 import (
 	"context"
 
-	"github.com/pirosiki197/emoine/pkg/model"
+	"github.com/pirosiki197/emoine/pkg/domain"
 )
 
 type Repository interface {
-	CreateEvent(ctx context.Context, event *model.Event) error
-	GetEvents(ctx context.Context) ([]model.Event, error)
-	GetEvent(ctx context.Context, id string) (*model.Event, error)
+	CreateEvent(ctx context.Context, event *domain.Event) error
+	GetEvents(ctx context.Context) ([]domain.Event, error)
+	GetEvent(ctx context.Context, id string) (*domain.Event, error)
 
-	CreateComment(ctx context.Context, comment *model.Comment) error
-	GetEventComments(ctx context.Context, eventID string) ([]model.Comment, error)
+	CreateComment(ctx context.Context, comment *domain.Comment) error
+	GetEventComments(ctx context.Context, eventID string) ([]domain.Comment, error)
 }
